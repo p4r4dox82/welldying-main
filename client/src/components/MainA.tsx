ea1b7bd197ab5b1ab06e36c0ef032f22275c685c
p4r4dox82 <81799661+p4r4dox82@users.noreply.github.com>
2021-08-02 23:02:22 +0900

    Update MainA.tsx

--- client/src/components/MainA.tsx
+++ client/src/components/MainA.tsx
@@ -5,15 +5,6 @@ import { imageUrl } from '../etc/config';
 function MainA() {
     return (
         <div style={{height: 'calc(100% - 150px)', backgroundImage: `url(${imageUrl('background.png')}`}} className='mainA'>
-            <div />
-            <div className='copyText'>
-                아직 전하지 못한 말이 많아서,
-                <br/>
-                미안하고, 사랑하고, 감사하다.
-            </div>
-            <div className='aboutButton'>
-                <Link to='/checklist'> &gt; 마이 웰다잉 서비스 바로가기 </Link>
-            </div>
         </div>
     );
 }
